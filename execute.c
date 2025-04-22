9abac9c95b880f8e168b2adfe1187f77f79574d5
geronimo304 <geronimonegreira06@gmail.com>
2025-04-22 13:19:12 +0000

    arreglos en betty, pero queda arreglar la cantidad de lineas del codigo

--- execute.c
+++ execute.c
@@ -5,44 +5,49 @@
  * @command: Command to execute
  * Return: Always 1 to continue shell loop
  */
-int execute_command(char *command) {
-  pid_t child_pid;
-  int status;
-  char *argv[2];
-  char *cmd_path;
-  char error_msg[1024];
-  char *program_name = "./hsh";
+int execute_command(char *command)
+{
+	pid_t child_pid;
+	int status;
+	char *argv[2];
+	char *cmd_path;
+	char error_msg[1024];
+	char *program_name = "./hsh";
 
-  cmd_path = find_command(command);
-  if (cmd_path == NULL) {
-    /* Format error message as required */
-    sprintf(error_msg, "%s: 1: %s: not found\n", program_name, command);
-    write(STDERR_FILENO, error_msg, strlen(error_msg));
-    return (1);
-  }
+	cmd_path = find_command(command);
+	if (cmd_path == NULL)
+	{
+		sprintf(error_msg, "%s: 1: %s: not found\n", program_name, command);
+		write(STDERR_FILENO, error_msg, strlen(error_msg));
+		return (1);
+	}
 
-  argv[0] = cmd_path;
-  argv[1] = NULL;
+	argv[0] = cmd_path;
+	argv[1] = NULL;
 
-  child_pid = fork();
-  if (child_pid == -1) {
-    perror("Error");
-    free(cmd_path);
-    return (1);
-  }
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("Error");
+		free(cmd_path);
+		return (1);
+	}
 
-  if (child_pid == 0) {
-    if (execve(cmd_path, argv, environ) == -1) {
-      sprintf(error_msg, "%s: 1: %s: not found\n", program_name, command);
-      write(STDERR_FILENO, error_msg, strlen(error_msg));
-      free(cmd_path);
-      exit(127);
-    }
-    exit(0);
-  } else {
-    waitpid(child_pid, &status, 0);
-    free(cmd_path);
-  }
-
-  return (1);
+	if (child_pid == 0)
+	{
+		if (execve(cmd_path, argv, environ) == -1)
+		{
+			sprintf(error_msg, "%s: 1: %s: not found\n", program_name, command);
+			write(STDERR_FILENO, error_msg, strlen(error_msg));
+			free(cmd_path);
+			exit(127);
+		}
+		exit(0);
+	}
+	else
+	{
+		waitpid(child_pid, &status, 0);
+		free(cmd_path);
+	}
+	return (1);
 }
--- path.c
+++ path.c
@@ -5,17 +5,20 @@
  * get_path - Get the PATH environment variable
  * Return: The PATH string or NULL if not found
  */
-char *get_path(void) {
-  int i;
-  char *path = NULL;
+char *get_path(void)
+{
+	int i;
+	char *path = NULL;
 
-  for (i = 0; environ[i]; i++) {
-    if (strncmp(environ[i], "PATH=", 5) == 0) {
-      path = environ[i] + 5;
-      break;
-    }
-  }
-  return (path);
+	for (i = 0; environ[i]; i++)
+	{
+		if (strncmp(environ[i], "PATH=", 5) == 0)
+		{
+			path = environ[i] + 5;
+			break;
+		}
+	}
+	return (path);
 }
 
 /**
@@ -23,49 +26,53 @@ char *get_path(void) {
  * @command: Command to find
  * Return: Full path to command or NULL if not found
  */
-char *find_command(char *command) {
-  char *path, *path_copy, *path_token, *file_path;
-  int command_len, directory_len;
-  struct stat buffer;
+char *find_command(char *command)
+{
+	char *path, *path_copy, *path_token, *file_path;
+	int command_len, directory_len;
+	struct stat buffer;
 
-  if (command == NULL)
-    return (NULL);
+	if (command == NULL)
+		return (NULL);
 
-  /* If command contains a slash, return as is if it exists */
-  if (strchr(command, '/') != NULL) {
-    if (stat(command, &buffer) == 0)
-      return (strdup(command));
-    return (NULL);
-  }
+	if (strchr(command, '/') != NULL)
+	{
+		if (stat(command, &buffer) == 0)
+			return (strdup(command));
+		return (NULL);
+	}
 
-  path = get_path();
-  if (path == NULL)
-    return (NULL);
+	path = get_path();
+	if (path == NULL)
+		return (NULL);
 
-  path_copy = strdup(path);
-  if (path_copy == NULL)
-    return (NULL);
+	path_copy = strdup(path);
+	if (path_copy == NULL)
+		return (NULL);
 
-  command_len = strlen(command);
-  path_token = strtok(path_copy, ":");
-  while (path_token != NULL) {
-    directory_len = strlen(path_token);
-    file_path = malloc(directory_len + command_len + 2);
-    if (file_path == NULL) {
-      free(path_copy);
-      return (NULL);
-    }
-    strcpy(file_path, path_token);
-    strcat(file_path, "/");
-    strcat(file_path, command);
+	command_len = strlen(command);
+	path_token = strtok(path_copy, ":");
+	while (path_token != NULL)
+	{
+		directory_len = strlen(path_token);
+		file_path = malloc(directory_len + command_len + 2);
+		if (file_path == NULL)
+		{
+			free(path_copy);
+			return (NULL);
+		}
+		strcpy(file_path, path_token);
+		strcat(file_path, "/");
+		strcat(file_path, command);
 
-    if (stat(file_path, &buffer) == 0) {
-      free(path_copy);
-      return (file_path);
-    }
-    free(file_path);
-    path_token = strtok(NULL, ":");
-  }
-  free(path_copy);
-  return (NULL);
+		if (stat(file_path, &buffer) == 0)
+		{
+			free(path_copy);
+			return (file_path);
+		}
+		free(file_path);
+		path_token = strtok(NULL, ":");
+	}
+	free(path_copy);
+	return (NULL);
 }
